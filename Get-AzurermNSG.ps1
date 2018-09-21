e8f2cee6d5a74844afb482d1bda74384aa0373a1
Bobbicus <robert.larkin@gmail.com>
2018-09-21 15:42:53 +0100

    updated NSG script new cool logic

--- Get-AzurermNSG.ps1
+++ Get-AzurermNSG.ps1
@@ -1,1 +1,39 @@
-﻿login-azuremrmaccount
+﻿   Function Get-Nsg
+        {
+        Param ([int]$NSGType) 
+                $NSGType
+                #Retrieve the Nic Name
+                $nic = $vm.NetworkProfile.NetworkInterfaces.id | Split-Path -Leaf
+                #Retrieve the effective NSGs for the given Nic
+                $nsg = Get-AzureRmEffectiveNetworkSecurityGroup -NetworkInterfaceName $nic -ResourceGroupName $VMRG 
+
+
+                #Get NSG Name
+                $NsgName = $nsg.NetworkSecurityGroup.Id | Split-Path -Leaf
+
+                #Get Subnet Name
+                $subnet = $nsg.Association.Subnet.Id | Split-Path -Leaf -ErrorAction SilentlyContinue
+                if ($subnet -eq $null)
+                {
+                    $subnet = "No rule at subnet level."
+                }
+
+                if ($NSGType -eq 1)
+                {
+                    #Filter out Default Rules and only Outbound rules first
+                    $NsgResults = $nsg.EffectiveSecurityRules | Where-Object {$_.Name -notlike "defaultSecurityRules*"} 
+                }
+                elseif($NSGType -eq 2)
+                {
+                    #Filter out Default Rules and only Outbound rules first
+                    $NsgResults = $nsg.EffectiveSecurityRules | Where-Object {$_.Name -notlike "defaultSecurityRules*" -and $_.Direction -eq 'Inbound'} 
+
+                }
+                elseif($NSGType -eq 3)
+                {
+                    #Filter out Default Rules and only Outbound rules first
+                    $NsgResults = $nsg.EffectiveSecurityRules | Where-Object {$_.Name -notlike "defaultSecurityRules*" -and $_.Direction -eq 'Outbound'} 
+                }
+
+                return $NsgResults
+        }
